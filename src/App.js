d70f2ee0827eb74d314e7589a7ef33248549092f
unknown <jtray@outlook.es>
2018-06-16 14:06:53 -0600

    buttons working after refresh

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     this.state = {
       original: [],
       formatted:[],
-      newName:'',
+      newName:'null',
       newCost:0,
     }
 
@@ -22,11 +22,23 @@ class App extends Component {
       this.setState({
         original:response.data,
         formatted:response.data.map((obj,ind)=>{return <div key = {obj.id+ind}><h3 className={obj.type}>{obj.name}: ${obj.amount}</h3></div>})
+        
       })
+      console.log(this.state.formatted)
         })}
      
   handleChangeName = (val) =>{this.setState({newName:val})}
-  handleChangeCost = (val) =>{this.setState({newCost:val})}      
+  handleChangeCost = (val) =>{this.setState({newCost:val})}
+
+  // with these next two i need to both do a this.state.formatted.push using setstate and do an axios.post
+  handleIncome =() =>{console.log('income')
+        axios.post(baseURL,{ type:"add", amount:this.state.newCost, name:this.state.newName})
+
+
+}
+  handleExpend = () =>{console.log('expend')
+  axios.post(baseURL,{ type:"sub", amount:this.state.newCost, name:this.state.newName})}    
+
   render() {
     return (
       <div className="App">
@@ -35,7 +47,7 @@ class App extends Component {
           <input onChange={(e) =>{this.handleChangeName(e.target.value)}}/><input onChange={(e) =>{this.handleChangeCost(e.target.value)}}/>
         </div>
         <div>
-          <button>Income</button><button>Expend</button>
+          <button onClick={()=>{this.handleIncome()}}>Income</button><button onClick={()=>{this.handleExpend()}}>Expend</button>
         </div>
         {this.state.formatted}
       </div>
